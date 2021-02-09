e0fadfc5ec67e13a7e4af0fb7969a2d03e736990
BivekKumarMali <bivekkumar@promactinfo.com>
2021-02-09 09:36:07 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dockerDotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
